c0c7a6df3f26ab3d07756e48c80c692d99d8d18d
betül kars <betulkars97@gmail.com>
2023-05-22 23:41:44 +0300

    Fsweb-s5g1 ödev çözümleri

--- src/index.js
+++ src/index.js
@@ -43,3 +43,60 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+const navItems = document.querySelectorAll("header nav a "); // bu kurala uyan hepsini alır. seleckt sadece ilk ögeyi alır.
+//navItems[0].textContent = siteContent["nav"]["nav-item-1"] 
+//navItems[1].textContent = siteContent["nav"]["nav-item-2"] 
+//navItems[2].textContent = siteContent["nav"]["nav-item-3"] 
+//navItems[3].textContent = siteContent["nav"]["nav-item-4"] 
+//navItems[4].textContent = siteContent["nav"]["nav-item-5"]  
+
+//navItems[0].setAttribute("class","italic") 
+//navItems[1].setAttribute("class","italic")
+//navItems[2].setAttribute("class","italic")
+//navItems[3].setAttribute("class","italic")
+//navItems[4].setAttribute("class","italic")
+
+navItems.forEach((item,index)=>{
+  let i= index+1;
+  item.textContent = siteContent["nav"]["nav-item-"+i] 
+  item.setAttribute("class","italic") //ekleme yapıyor.
+  })
+
+const logoImg = document.querySelector ("header img");//id den de gidilebilir.
+logoImg.setAttribute("src",siteContent.images["logo-img"])  //sitecontent ne yapıyor? Hocaya sor.
+
+document.getElementById("cta-img").src=siteContent.images["cta-img"]; //2.yol
+document.getElementById("middle-img").src=siteContent.images["accent-img"]; //2.yol
+
+const mainText = document.querySelector(".cta-text"); //içinde 2 tane şey var. h1 ve button. Bunları için ayrı ayrı oluşturmaktansa aşağıdaki gibi yapabiliriz!
+mainText.querySelector("h1").textContent = siteContent.cta.h1;  
+mainText.querySelector("button").textContent = siteContent.cta.button;
+
+const topContent = document.querySelectorAll(".top-content .text-content");//sınıf olduğu için noktalı yazdık. text contentten 2 tane var. Bunu bir array gibi düşünebilirsin.
+topContent[0].querySelector("h4").textContent = siteContent["ana-içerik"]["özellikler-h4"]; //topcontenrin ilk elemanına ualştım. quaryselector ile içerisinde h4 yazdım. yazısını değiştireceğim için textconteent dedim.
+topContent[0].querySelector("p").textContent = siteContent["ana-içerik"]["özellikler-içerik"]; // köşeli parantezlerde yazanları yukarıdan seçtim
+topContent[1].querySelector("h4").textContent = siteContent["ana-içerik"]["hakkımızda-h4"];
+topContent[1].querySelector("p").textContent = siteContent["ana-içerik"]["hakkımızda-içerik"];
+
+const bottomContent = document.querySelectorAll(".bottom-content  h4");
+
+bottomContent[0].textContent = siteContent["ana-içerik"]["servisler-h4"];
+bottomContent[1].textContent = siteContent["ana-içerik"]["ürünler-h4"];
+bottomContent[2].textContent = siteContent["ana-içerik"]["vizyon-h4"];
+
+const bottomContent_p = document.querySelectorAll(".bottom-content  p");
+
+bottomContent_p[0].textContent = siteContent["ana-içerik"]["servisler-içeriği"];
+bottomContent_p[1].textContent = siteContent["ana-içerik"]["ürünler-içeriği"];
+bottomContent_p[2].textContent = siteContent["ana-içerik"]["vizyon-içeriği"];
+
+document.querySelector(".contact h4").textContent=siteContent.iletisim["iletişim-h4"];
+const contact = document.querySelectorAll(".contact p")
+contact[0].textContent=siteContent.iletisim.telefon;
+contact[1].textContent=siteContent.iletisim.adres;
+contact[2].textContent=siteContent.iletisim.email;
+document.querySelector("footer a").textContent=siteContent.footer.copyright;
+
+document.querySelector("footer a").classList.add("bold");
+//document.querySelector("footer a").setAttribute("class","bold");
